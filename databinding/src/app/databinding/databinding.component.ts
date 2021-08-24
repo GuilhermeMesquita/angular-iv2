4bf8dd891dad19f41874a92dedc59cd9b499d14b
Guilherme Rocha <guilhermemesquitarch@gmail.com>
2021-08-24 10:32:24 -0300

    finished property binding samples

--- databinding/src/app/databinding/databinding.component.ts
+++ databinding/src/app/databinding/databinding.component.ts
@@ -9,6 +9,8 @@ export class DatabindingComponent implements OnInit {
 
   url: string = "https://faculdadeiv2.com.br";
   cursoAngular: boolean = true;
+  imageURL: string = "https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png"
+  imageWidth: number = 50;
 
   getValor() {
     return 1;
